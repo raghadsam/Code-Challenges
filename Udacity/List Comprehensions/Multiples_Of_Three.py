6f48e770101d9df32d6c71b79328243cf7043c2d
raghadsam <raghad66samrout@gmail.com>
2022-02-04 23:26:57 +0200

    multiple of three

---
+++ Udacity/List Comprehensions/Multiples_Of_Three.py
@@ -0,0 +1,5 @@
+# Quiz: Multiples of Three
+# Use a list comprehension to create a list multiples_3 containing the first 20 multiples of 3.
+
+multiples_3 = [num * 3 for num in range(1,21)]
+print(multiples_3)
